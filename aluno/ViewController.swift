ce9ed061acb7b7b417e51b583cfe3b834219daf6
Urvi Sehgal <usehgal140@gmail.com>
2022-08-18 13:08:41 -0400

    working on making code for "making post functional"

--- aluno/ViewController.swift
+++ aluno/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func photoLibrary(_ sender: Any) {
+    }
+    
 }
 
